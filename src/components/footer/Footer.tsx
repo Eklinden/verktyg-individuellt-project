d179cb4b60451b33a101e63ef1c73fa776064607
Linus Eklind <76260367+Eklinden@users.noreply.github.com>
2022-09-28 14:09:07 +0200

    footer update

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,20 @@
+import facebook from '../../images/facebook.svg';
+import instagram from '../../images/insta.svg';
+import linkedin from '../../images/linkedin.svg';
+import twitter from '../../images/twitter.svg';
 import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="footer">
+      <h2>Sociala Medier</h2>
+      <section>
+        <a href=""><img src={facebook} alt="" /></a>
+        <a href=""><img src={twitter} alt="" /></a>
+        <a href=""><img src={instagram} alt="" /></a>
+        <a href=""><img src={linkedin} alt="" /></a>
+      </section>
+    </footer>
   )
 }
 
